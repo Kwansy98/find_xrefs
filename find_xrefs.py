fc10bc15536ca92c43647ac11961a343a13e5dc5
hambaga <adamn1024@outlook.com>
2025-02-16 01:31:37 +0800

    xrefs to

--- find_xrefs.py
+++ find_xrefs.py
@@ -22,11 +22,12 @@ blacklist = [
 ]
 
 default_from = 1
+default_to = 0
 
 class XrefFinderPlugin(idaapi.plugin_t):
     flags = idaapi.PLUGIN_UNL
     comment = "Xref Finder Plugin"
-    help = "print xrefs from"
+    help = "print xrefs to and xrefs from"
     wanted_name = "Xref Finder"
     wanted_hotkey = "Ctrl-Shift-F"
     
@@ -42,24 +43,23 @@ class XrefFinderPlugin(idaapi.plugin_t):
     def show_dialog(self):
         class XrefFinderForm(ida_kernwin.Form):
             def __init__(self):
+                self.depth_to = default_to
                 self.depth_from = default_from
-
                 current_ea = ida_kernwin.get_screen_ea()
                 default_func_name = idc.ida_funcs.get_func_name(current_ea) or ""
-
                 ida_kernwin.Form.__init__(self, r"""STARTITEM 0
 BUTTON YES* OK
 Xref Finder
-
 {FormChangeCb}
 <##Function name:{iFuncName}>
+<##xrefs to depth:{iDepthTo}>
 <##xrefs from depth:{iDepthFrom}>
 """, {
                     'iFuncName': ida_kernwin.Form.StringInput(value=default_func_name),
+                    'iDepthTo': ida_kernwin.Form.NumericInput(tp=ida_kernwin.Form.FT_DEC, value=self.depth_to),
                     'iDepthFrom': ida_kernwin.Form.NumericInput(tp=ida_kernwin.Form.FT_DEC, value=self.depth_from),
                     'FormChangeCb': ida_kernwin.Form.FormChangeCb(self.OnFormChange),
                 })
-
             def OnFormChange(self, fid):
                 return 1
 
@@ -68,8 +68,9 @@ Xref Finder
         ok = form.Execute()
         if ok == 1:
             func_name = form.iFuncName.value
+            depth_to = form.iDepthTo.value
             depth_from = form.iDepthFrom.value
-            self.find_xrefs(func_name, depth_from)
+            self.find_xrefs(func_name, depth_to, depth_from)
         form.Free()
 
     def find_subfunctions(self, func_name, depth, current_depth=0, path=None):
@@ -77,14 +78,11 @@ Xref Finder
             path = [func_name]
         else:
             path.append(func_name)
-
         if current_depth > depth:
             return
-        
         for black in blacklist:
             if black in func_name:
                 return
-
         func_ea = idc.get_name_ea_simple(func_name)
         if func_ea == idc.BADADDR:
             path.pop()
@@ -112,9 +110,7 @@ Xref Finder
             # else:
             #     print(f"Function {func_name} not found!!")
             return
-
         called_functions = set()
-
         for (startea, endea) in idautils.Chunks(func_ea):
             for head in idautils.Heads(startea, endea):
                 if idc.is_code(idc.get_full_flags(head)):
@@ -125,14 +121,38 @@ Xref Finder
                             ref_name = hex(ref)
                         if ref_name and ref_name != func_name:
                             called_functions.add((ref, ref_name))
-
         if current_depth == depth or not called_functions:
             print(" -> ".join(path[:]))
         else:
             for addr, name in called_functions:
                 self.find_subfunctions(name, depth, current_depth + 1, path[:])
 
-    def find_xrefs(self, func_name, depth_from):
+    def find_parentfunctions(self, func_name, depth, current_depth=0, path=None):
+        if path is None:
+            path = [func_name]
+        else:
+            path.append(func_name)
+        if current_depth > depth:
+            return
+        func_ea = idc.get_name_ea_simple(func_name)
+        if func_ea == idc.BADADDR:
+            print(f"Function {func_name} not found.")
+            return
+        calling_functions = set()
+        for ref in idautils.CodeRefsTo(func_ea, 0):
+            ref_name = idc.get_func_name(ref)
+            if not ref_name:
+                ref_name = hex(ref)
+            if ref_name and ref_name != func_name:
+                calling_functions.add((ref, ref_name))
+        if current_depth == depth or not calling_functions:
+            print(" -> ".join(reversed(path)))
+        else:
+            for addr, name in calling_functions:
+                self.find_parentfunctions(name, depth, current_depth + 1, path[:])
+
+    def find_xrefs(self, func_name, depth_to, depth_from):
+        self.find_parentfunctions(func_name, depth_to)
         self.find_subfunctions(func_name, depth_from)
 
 def PLUGIN_ENTRY():
